cb008f4e6094df5b0fa8f8dfdb17049a7225cde3
Daniil Roman <danroman17397@gmail.com>
2018-02-03 16:26:22 +0300

    LinkedList

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -15,7 +15,7 @@ function DoubleList() {
 
 DoubleList.prototype.add = function (value) {
     var node = new Node(value);
-    if(!this._length){
+    if(this._length){
         this.tail.next = node;
         node.previous = this.tail;
         this.tail = node;
@@ -28,7 +28,7 @@ DoubleList.prototype.add = function (value) {
     return node;
 };
 
-DoubleList.prototype.searchNodeAt = function (position) {
+DoubleList.prototype.get = function (position) {
     var currentNode = this.head,
         length = this._length,
         count = 1,
@@ -65,9 +65,13 @@ DoubleList.prototype.remove = function (position) {
         this.tail = this.tail.previous;
         this.tail.next = null;
     } else {
-        while (count < position){
-            currentNode = currentNode.next;
+        while (true){
+            //currentNode = currentNode.next;
             count++;
+            if(count<position){
+                currentNode = currentNode.next;
+            }else break
+
         }
 
 
@@ -86,6 +90,71 @@ DoubleList.prototype.remove = function (position) {
 
 };
 
+// DoubleList.prototype.set = function (position, value) {
+//     length = this._length;
+//     if(position<0||position>length)
+// }
+
+DoubleList.prototype.push = function () {
+  for(var i=0;i<arguments.length;i++){
+      var node = new Node(arguments[i]);
+      this.tail.next = node;
+      node.previous = this.tail;
+      this.tail = node;
+      this._length++;
+  }
+};
+DoubleList.prototype.unshift = function () {
+    for (var i = 0; i < arguments.length; i++) {
+        var node = new Node(arguments[i]);
+        this.head.previous = node;
+        node.next = this.head;
+        this.head = node;
+        this._length++;
+    }
+};
+
+DoubleList.prototype.pop = function () {
+    this.tail = this.tail.previous;
+    this.tail.next = null;
+    this._length--;
+};
+
+DoubleList.prototype.shift = function () {
+    this.head = this.head.next;
+    this.head.previous = null;
+    this._length--;
+};
+
+DoubleList.prototype.contains = function (value) {
+    for(var i = 1; i <= this._length; i++){
+        if(value === this.get(i).data) return true
+    }
+    return false
+};
+
+DoubleList.prototype.toString = function () {
+    listStr = "";
+    for(var i = 1; i <= this._length; i++){
+        data = this.get(i).data;
+        listStr += JSON.stringify(data) + " => ";
+    }
+    return listStr;
+};
+
+DoubleList.prototype.reverse = function () {
+    tempList = new DoubleList();
+
+    for(i = 1;i<=this._length;i++){
+        tempList.unshift(this.get(i).data);
+    }
+
+    this.tail = tempList.tail;
+    this.head = tempList.head;
+
+};
+
+
 
 
 
@@ -93,8 +162,22 @@ var list = new DoubleList("fsdf","dsfdf");
 console.log(list._length);
  list.add("sadasd");
  list.add(324);
-// list.add({a:'asd',b:'asd'});
+ list.add({a:'asd',b:'assad'});
+// for (var i = 1; i <= list._length; i++) {
+//     console.log(list.get(i));
+// }
+// console.log(list.get(5));
+// console.log(list.get(2));
+// console.log(list.remove(2));
 console.log(list._length);
-console.log(list.searchNodeAt(1));
-console.log(list.remove(2));
+list.push("fdfdsf",7665);
 console.log(list._length);
+list.unshift({ad:"dasa",q:3143},1123,'asdaw');
+console.log(list._length);
+console.log(list.get(10));
+list.pop();
+console.log(list._length);
+console.log(list.contains("2asdaw"));
+console.log(list.toString());
+list.reverse();
+console.log(list.toString());
---
+++ test2.js
@@ -0,0 +1,6 @@
+if (0) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
